47c2ba34d345440783298937d3988d254230b5f5
Manu Phatak <manu@phatak.io>
2026-08-11 13:53:15 -0500

    Scaffold bot

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,150 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+import { z } from 'zod'
+
+const BodySchema = z.object({
+	attachments: z.array(
+		z.discriminatedUnion('type', [
+			z.object({
+				type: z.literal('image'),
+				url: z.httpUrl(),
+			}),
+			z.object({
+				type: z.literal('video'),
+				url: z.httpUrl(),
+				preview_url: z.httpUrl(),
+			}),
+			z.object({
+				type: z.literal('file'),
+				file_id: z.string(),
+			}),
+			z.object({
+				type: z.literal('location'),
+				name: z.string(),
+				lat: z.string().regex(/^-?\d+(\.\d+)?$/),
+				lng: z.string().regex(/^-?\d+(\.\d+)?$/),
+			}),
+			z.object({
+				type: z.literal('emoji'),
+				placeholder: z.string(),
+				charmap: z.array(z.tuple([z.number(), z.number()])),
+			}),
+
+			z.object({
+				type: z.literal('reply'),
+				reply_id: z.string(),
+				base_reply_id: z.string(),
+			}),
+
+			z.object({
+				type: z.literal('mentions'),
+				user_ids: z.array(z.string()),
+				loci: z.array(z.tuple([z.number(), z.number()])),
+			}),
+			z.object({
+				type: z.literal('poll'),
+				poll_id: z.string(),
+			}),
+			z.object({
+				type: z.literal('event'),
+				event_id: z.string(),
+				view: z.enum(['brief', 'full']),
+			}),
+			z.object({
+				type: z.literal('copilot'),
+				message_id: z.string(),
+				part_id: z.string(),
+				prompt_sender: z.string(),
+			}),
+			z.object({
+				type: z.literal('partial_image'),
+				id: z.string(),
+				content: z.string(),
+			}),
+		])
+	),
+	// avatar_url: z.string().url().optional(),
+	// created_at: z.string().datetime().optional(),
+	// group_id: z.string().uuid().optional(),
+	// id: z.string().uuid().optional(),
+	// name: z.string().max(100).optional(),
+	sender_id: z.string().max(100),
+	sender_type: z.enum(['system', 'service']),
+	source_guid: z.string(),
+	system: z.boolean(),
+	text: z.string(),
+	// user_id: z.string().max(100).optional(),
+	// __IMTMETHOD__: z.literal('POST').optional(),
+})
+
+const createSuccessResponse = () =>
+	new Response(JSON.stringify({ success: true }), {
+		status: 200,
+		headers: { 'Content-Type': 'application/json' },
+	})
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World!");
+		if (request.method !== 'POST') {
+			return new Response('Method not allowed', { status: 405 })
+		}
+
+		let body: z.infer<typeof BodySchema>
+
+		try {
+			const rawBody = await request.json()
+			body = BodySchema.parse(rawBody)
+		} catch (err) {
+			console.error('🚀 ~ err:', err)
+			if (err instanceof z.ZodError) {
+				return new Response(
+					JSON.stringify({ error: err.message, issues: err.issues }),
+					{
+						status: 400,
+						headers: { 'Content-Type': 'application/json' },
+					}
+				)
+			}
+			return new Response(JSON.stringify({ error: 'Invalid JSON' }), {
+				status: 400,
+				headers: { 'Content-Type': 'application/json' },
+			})
+		}
+
+		// event is starting
+		if (
+			body.sender_type === 'service' &&
+			body.sender_id === 'calendar' &&
+			body.attachments[0]?.type === 'event' &&
+			body.text.endsWith('is starting now')
+		) {
+			console.log('🚀 ~ event is starting:', body)
+			return createSuccessResponse()
+		}
+		// event is created
+		if (
+			body.attachments.length === 1 &&
+			body.attachments[0].type === 'event' &&
+			body.text.includes('created event')
+		) {
+			console.log('🚀 ~ event is created:', body)
+			return createSuccessResponse()
+		}
+
+		// event is cancelled
+		if (
+			body.attachments.length === 1 &&
+			body.attachments[0].type === 'event' &&
+			body.sender_id === 'system' &&
+			body.system === true &&
+			body.text.includes('cancelled')
+		) {
+			console.log('🚀 ~ event is cancelled:', body)
+			return createSuccessResponse()
+		}
+
+		console.log('🚀 ~ SKIPPING:', body)
+		return createSuccessResponse()
 	},
-} satisfies ExportedHandler<Env>;
+} satisfies ExportedHandler<Env>
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,27 +3,50 @@ import {
 	createExecutionContext,
 	waitOnExecutionContext,
 	SELF,
-} from "cloudflare:test";
-import { describe, it, expect } from "vitest";
-import worker from "../src/index";
+} from 'cloudflare:test'
+import { describe, it, expect } from 'vitest'
+import worker from '../src/index'
 
 // For now, you'll need to do something like this to get a correctly-typed
 // `Request` to pass to `worker.fetch()`.
-const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+const IncomingRequest = Request<unknown, IncomingRequestCfProperties>
 
-describe("Hello World worker", () => {
-	it("responds with Hello World! (unit style)", async () => {
-		const request = new IncomingRequest("http://example.com");
-		// Create an empty context to pass to `worker.fetch()`.
-		const ctx = createExecutionContext();
-		const response = await worker.fetch(request, env, ctx);
-		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
+describe('given an event is starting message', () => {
+	const body = {
+		attachments: [
+			{
+				event_id: 'e22aa9f6f77346949ac0903e7fdd116d',
+				view: 'brief',
+				type: 'event',
+			},
+		],
+		avatar_url:
+			'https://i.groupme.com/204x204.png.ae6fd52515e747c88db501db9e9fcfd9',
+		created_at: '2026-07-24T11:59:01.000Z',
+		group_id: '116072458',
+		id: '178489434135707461',
+		name: 'GroupMe Calendar',
+		sender_id: 'calendar',
+		sender_type: 'service',
+		source_guid: 'fbc828e06984013f3182425eb3f39ddd',
+		system: false,
+		text: "'Fri v5' is starting now",
+		user_id: 'calendar',
+		__IMTMETHOD__: 'POST',
+	}
 
-	it("responds with Hello World! (integration style)", async () => {
-		const response = await SELF.fetch("https://example.com");
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
-});
+	it('responds with a 200 status', async () => {
+		const response = await SELF.fetch('https://example.com', {
+			method: 'POST',
+			body: JSON.stringify(body),
+		})
+		expect(response.status).toBe(200)
+	})
+	it('responds with a 200 status', async () => {
+		const response = await SELF.fetch('https://example.com', {
+			method: 'POST',
+			body: JSON.stringify(body),
+		})
+		expect(response.status).toBe(200)
+	})
+})
